ca2d27bedc4d8b8d5eb57d5299216a8563360e47
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2022-05-02 19:35:47 -0300

    Entity

---
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -0,0 +1,103 @@
+package com.devsuperior.movieflix.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+
+@Entity(name="tb_user")
+public class User {
+	
+	Long id;
+	String name;
+	String email;
+	String password;
+	
+	List<Role> roles = new ArrayList<>();
+	
+	List<Review> reviews = new ArrayList<>(); 
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public User(User user) {
+		id = user.id;
+		name = user.name;
+		email = user.email;
+		password = user.password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
